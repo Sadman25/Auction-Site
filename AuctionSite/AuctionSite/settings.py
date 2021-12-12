30904f1f45d6d0803a64a877b5b45121558dbff8
Sadman25 <shadman.saqif@northsouth.edu>
2021-12-12 19:30:29 +0600

    #3 Step 6: create auction page created and fixed urls among apps

--- AuctionSite/AuctionSite/settings.py
+++ AuctionSite/AuctionSite/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts','crispy_forms',
+    'accounts',
+    'crispy_forms',
+    'auction',
 ]
 
 MIDDLEWARE = [
--- AuctionSite/auction/views.py
+++ AuctionSite/auction/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homePage(request):
+    context = {}
+    return render (request, 'homepage.html',context)
+
+def createAuction(request):
+    context = {}
+    return render (request, 'homepage.html',context)
