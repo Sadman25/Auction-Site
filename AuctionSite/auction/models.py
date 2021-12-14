46053f72bb1e9ce34c616e6afacb5b41208fdb40
Sadman25 <shadman.saqif@northsouth.edu>
2021-12-14 21:59:54 +0600

    #3 Step 17: Database remigrated for add some new fields

--- AuctionSite/auction/models.py
+++ AuctionSite/auction/models.py
@@ -6,6 +6,14 @@ from django.contrib.auth.models import User
 from django.db.models.fields import DateField
 # Create your models here.
 
+
+class category (models.Model):
+    category = models.CharField(max_length=20, unique=True)
+    
+    def __str__(self) -> str:
+        return 'Catergory: ' + self.category
+
+
 class status (models.Model):
     status = models.CharField(max_length=20, unique=True)
     
@@ -16,10 +24,11 @@ class product(models.Model):
     product_name= models.CharField(max_length=50, blank=False)
     product_description=models.CharField(max_length=500,null=False, blank=False)
     cover_photo=models.ImageField(null=False, blank=False)
-    starting_bid = models.CharField(max_length=10, null=False, blank=False)
+    starting_bid = models.PositiveIntegerField(null=False, blank=False)
     deadline = models.DateTimeField()
     owner = models.ForeignKey(User,on_delete=models.CASCADE,default=None)
     product_status = models.ForeignKey(status, null=True, on_delete= models.SET_NULL)
+    product_category = models.ForeignKey(category, null=True, on_delete= models.SET_NULL)
 
     def __str__(self) -> str:
         return 'Product: '+self.product_name + ' Owner: '+ self.owner.username
